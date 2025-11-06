6a71005fed40b2530c09c74247f15c200a357872
Melissa <melissalarras5@gmail.com>
2025-11-06 18:51:14 +0100

    ajout classe ray et HitRecord

---
+++ core/HitRecord.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "Vec3.hpp"
+#include "Material.hpp"
+
+struct HitRecord {
+    double distanceFromRayOrigin; // Distance entre l'origine du rayon et le point d'impact
+    Vec3 impactPoint; // Coordonnées exactes du point d'impact dans la scène
+    Vec3 surfaceNormal; // Normale de la surface au point d'impact
+    const Material* material; // Matériau de l'objet touché
+};
