a876ce86054feda71912208840fe7c05b0f91a51
Daiiruin <95864317+Daiiruin@users.noreply.github.com>
2025-11-07 09:23:18 +0100

    [FIX] main file (#17)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "scene/SceneLoader.hpp"
+#include "render/Renderer.hpp"
 
 int main() {
-    std::cout << "Raytracer starting point!" << std::endl;
+    try {
+        Scene scene = loadScene("scene.json");
+        std::cout << "Scène chargée avec succès." << std::endl;
+
+        Renderer renderer;
+        renderer.maxDepth = 3;
+
+        std::cout << "Démarrage du rendu..." << std::endl;
+        renderer.render(scene);
+
+        std::cout << "Rendu terminé avec succès !" << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Erreur : " << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
