c6a1b0bbc72303216a601a304f3053348e065da6
Melissa <melissalarras5@gmail.com>
2025-11-07 10:10:31 +0100

    modif

--- core/HitRecord.hpp
+++ core/HitRecord.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "Vec3.hpp"
 #include "Material.hpp"
-
+//structure clé du moteur de rendu
 struct HitRecord {
     double distanceFromRayOrigin; // Distance entre l'origine du rayon et le point d'impact
     Vec3 impactPoint; // Coordonnées exactes du point d'impact dans la scène
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "scene/SceneLoader.hpp"
+<<<<<<< Updated upstream
 #include "renderer/Renderer.hpp"
+=======
+#include "render/Renderer.hpp"
+#include <chrono>
+>>>>>>> Stashed changes
 
 int main() {
+     
     try {
+        // --- Mesure du temps de rendu
+    auto start = std::chrono::high_resolution_clock::now();
         Scene scene = loadScene("scene.json");
         std::cout << "Scène chargée avec succès." << std::endl;
 
@@ -13,11 +21,16 @@ int main() {
         renderer.render(scene);
 
         std::cout << "Rendu terminé avec succès !" << std::endl;
+        // --- Fin du rendu
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
     }
     catch (const std::exception& e) {
         std::cerr << "Erreur : " << e.what() << std::endl;
         return 1;
     }
+ 
 
+    std::cout << "Rendu terminé en " << elapsed.count() << " secondes.\n";
     return 0;
 }
